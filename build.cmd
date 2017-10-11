71e27e32d245b11c86310d9ebdfbe83f948ee952
Anatoly Volodko <Anatoly.V.Volodko@gmail.com>
2017-10-11 20:34:28 +0300

    Update Castle and Moq

--- build.cmd
+++ build.cmd
@@ -11,7 +11,7 @@ REM Find the most recent 32bit MSBuild.exe on the system. Require v12.0 (install
 REM is not supported. Also handle x86 operating systems, where %ProgramFiles(x86)% is not defined. Always quote the
 REM %MSBuild% value when setting the variable and never quote %MSBuild% references.
 set MSBuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
-if not exist %MSBuild% @set MSBuild="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
+if not exist %MSBuild% @set MSBuild="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MsBuild.exe"
 set FullBuild=0
 
 REM with no switches, run the full set of tests.
